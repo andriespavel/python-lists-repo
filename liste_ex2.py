12de5f0a42163beff6f11b53ae4e3c4c59400669
andriespavel <andriespavel7@gmail.com>
2026-01-27 20:32:22 +0100

    Add .gitignore

---
+++ liste_ex2.py
@@ -0,0 +1,15 @@
+numbers= [10,20,30,40]
+
+for number in numbers:
+    print(number)
+
+
+
+state_ale_lumii = ['Franta','Italia','Belgia']
+
+print(state_ale_lumii[-1])
+
+lista_secreta_fructe = ['mure','mere','capsuni','banane']
+
+#lungimea
+
--- liste_primul_exemplu.py
+++ liste_primul_exemplu.py
@@ -1,1 +1,19 @@
-print("Liste")
+numbers= [10,20,30,40]
+
+print(numbers)
+
+print(numbers[3]) #40
+
+numbers[2] = 300
+
+print(numbers)
+#add elements
+numbers.append(100)
+numbers.append(200)
+
+print(numbers)
+
+#remove elements by id
+
+numbers.pop(1)
+print(numbers)
