a9143de5040ee6d802afeb4b4d795576c60fa603
andriespavel <andriespavel7@gmail.com>
2026-01-27 21:14:18 +0100

    Add remove exercise

---
+++ remove_exerecise.py
@@ -0,0 +1,15 @@
+numbers = [10,20,30,40]
+
+#delete by value
+#numbers.remove(20)
+#print(numbers)
+
+#delete by index
+#removed_element = numbers.pop(1)
+#print(numbers)
+#print(removed_element)
+
+#delete entire list
+
+numbers.clear()#
+print(numbers)
