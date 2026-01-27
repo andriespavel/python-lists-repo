50b6d220050d542fcf678c25420742be9ca13ae7
andriespavel <andriespavel7@gmail.com>
2026-01-27 20:55:27 +0100

    Add diff

---
+++ liste_ex3.py
@@ -0,0 +1,7 @@
+state_ale_lumii = ['Franta','Italia','Belgia']
+
+state_noi = ['Spania','Germania']
+state_ale_lumii.extend(state_noi)
+state_ale_lumii.append(state_noi)#adauga lista in alta lista
+
+print(state_ale_lumii)
