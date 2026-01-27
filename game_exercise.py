a7f7723ed1023d4d635dda4c9e481129359f3fe4
andriespavel <andriespavel7@gmail.com>
2026-01-27 21:51:24 +0100

    Add game exercise

--- game_exercise.py
+++ game_exercise.py
@@ -1,3 +1,5 @@
+import random
+
 game_images = [
     """
     _______
@@ -24,3 +26,32 @@ game_images = [
 ---.__(___)
 """
 ]
+
+computer_choice = random.randint(0,2)
+
+user_choice = int(input("Alege:0 = Rock,1 =Paper, 2= Scissors:"))
+
+print("CALCULATOR:",game_images[computer_choice])
+print("UTILIZATOR:",game_images[user_choice])
+
+if user_choice == computer_choice:
+    print("Egalitate!")
+elif user_choice == 0 and computer_choice ==2:
+    print("Ai gastigat!")
+elif user_choice == 2 and computer_choice == 1:
+    print("Ai gastigat!")
+elif user_choice == 1 and computer_choice == 0:
+    print("Ai gastigat!")
+else:
+    print("Ai pierdut!")
+
+
+
+
+
+
+
+
+
+
+
